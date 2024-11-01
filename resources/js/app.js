64ea894b917ff0a74c6da201a1fb014a2d3448fd
Hassan Agmir <hagmir6@gmail.com>
2024-11-01 22:04:08 +0100

    add swiperjs

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,40 +9,28 @@ Alpine.start()
 import Swiper from 'swiper';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
-document.addEventListener('DOMContentLoaded', function () {
-    const swiper = new Swiper('.book-swiper', {
-        modules: [Navigation, Pagination, Autoplay],
-        slidesPerView: 3,
-        // spaceBetween: 30,
-        loop: true,
-        autoplay: {
-            delay: 5000,
+const swiper = new Swiper('.book-swiper', {
+    modules: [Navigation, Pagination, Autoplay],
+    slidesPerView: 3,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+
+
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 3,
         },
 
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
+        1600: {
+            slidesPerView: 4,
         },
-
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-        breakpoints: {
-            640: {
-                slidesPerView: 1,
-            },
-            768: {
-                slidesPerView: 2,
-            },
-            1200: {
-                slidesPerView: 3,
-            },
-
-            1600: {
-                slidesPerView: 4,
-            },
-        }
-    });
+    }
 });
