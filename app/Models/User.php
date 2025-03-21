ac59868f02879818bdb2002495795791d15144f9
Agmir Hassane <hagmir6@gmail.com>
2025-03-21 22:20:49 +0000

    Update users seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ use Illuminate\Notifications\Notifiable;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Panel;
 
 class User extends Authenticatable implements FilamentUser, HasName, HasMedia
 {
