9c0689109286ffc23020bc5c17b35266947e7fa4
Agmir Hassane <hagmir6@gmail.com>
2025-03-22 15:48:06 +0000

    Add search button in blog details

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -14,6 +14,15 @@
         z-index: 50;
     }
 
+    figure a img{
+        width: 100%;
+        height: auto;
+        margin-top: 15px;
+        margin-bottom: 15px;
+        border-radius: 10px
+
+    }
+
     .progress-bar {
         height: 4px;
         background: linear-gradient(to right, #3b82f6, #8b5cf6);
@@ -187,7 +196,7 @@
                     <a href="https://api.whatsapp.com/send?text={{ $postTitle }}%20{{ $shareUrl }}" target="_blank"
                         class="bg-white text-green-500 hover:bg-green-500 hover:text-white p-3 rounded-full shadow-lg transition duration-300"
                         aria-label="Share on WhatsApp">
-                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                             <path
                                 d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347">
                             </path>
@@ -220,7 +229,7 @@
                 <script>
                     function copyToClipboard(link) {
                         navigator.clipboard.writeText(link).then(() => {
-                            alert('Link copied to clipboard!');
+
                         }).catch(err => {
                             console.error('Failed to copy: ', err);
                         });
